178831ac55e7d712b4ff8aa437ef44c180cad377
Maicor Borges <maicorborges@gmail.com>
2023-05-04 13:36:27 -0300

    cart view

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,13 +1,19 @@
+
 import React from 'react'
 import cart from './assets/icon_shopping_cart.svg';
-
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext.js';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
+    const { totalQuantity } = useContext(CartContext)
+
+
     return (
-        <div>
-            <img src={cart} alt="cart-widget" />
-            0
-        </div>
+        <Link to='/cart' className='CartWidget' style={{display: totalQuantity > 0 ? 'block' : 'none'}}>
+            <img classname="Cartimg" src={cart} alt="cart-widget" />
+            {totalQuantity}
+        </Link>
     )
 }
 
