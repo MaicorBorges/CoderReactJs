e647a989454604929035d148b792d4b71cb45148
Maicor Borges <maicorborges@gmail.com>
2023-05-10 14:25:21 -0300

    order

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,15 +1,11 @@
 import { useState, useContext } from 'react';
 import { CartContext } from '../../context/CartContext.js';
-import { Timestamp, writeBatch, } from 'firebase/firestore';
+import { Timestamp } from 'firebase/firestore';
 
 
 import {
-    documentId,
     collection,
-    getDocs,
-    addDoc,
-    query,
-    where
+    addDoc
 } from 'firebase/firestore';
 
 
@@ -33,48 +29,18 @@ const Checkout = () => {
                     name, phone, email
                 },
                 items: cart,
-                total: total,
+                total: total(),
                 date: Timestamp.fromDate(new Date())
             }
 
-            const batch = writeBatch(db)
-
-            const outOfStock = []
-
-            const ids = cart.map(prod => prod.id)
-
-            const productsRef = collection(db, 'productos')
-
-            const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), 'in', ids)))
-
-            const { docs } = productsAddedFromFirestore
-
-            docs.forEach(doc => {
-                const dataDoc = doc.data()
-                const stockDb = dataDoc.stock
-
-                const productAddedToCart = cart.find(prod => prod.id === doc.id)
-                const prodQuantity = productAddedToCart?.quantity
-
-                if (stockDb >= prodQuantity) {
-                    batch.update(doc.ref, { stock: stockDb - prodQuantity })
-                } else {
-                    outOfStock.push({ id: doc.id, ...dataDoc })
-                }
-            })
-
-            if (outOfStock.length === 0) {
-                await batch.commit()
-
+            console.log(objOrder)
                 const orderRef = collection(db, 'orders')
 
                 const orderAdded = await addDoc(orderRef, objOrder)
 
                 setOrderId(orderAdded.id)
                 clearCart()
-            } else {
-                console.error('Hay productos que estan fuera de stock')
-            }
+            
 
         } catch (error) {
             console.log(error)
@@ -100,5 +66,3 @@ const Checkout = () => {
 }
 
 export default Checkout
-
-
